6347e913b44c826a3a21cedbf546a5cfdc2293be
ThanhHuyen2212 <huyennguyen850.vn@gmail.com>
2022-05-06 16:31:34 +0700

    Huyen push code 06-05

--- Admin/IngredientsManager/Controller/IngredientAddController.java
+++ Admin/IngredientsManager/Controller/IngredientAddController.java
@@ -1,4 +1,24 @@
 package Main.Admin.IngredientsManager.Controller;
 
+import Main.Entity.Element.Ingredient;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class IngredientAddController extends MasterController{
+
+    @FXML
+    private TextField nameTxtField;
+    @FXML
+    private TextField typeTxtField;
+    @FXML
+    private TextField priceTxtField;
+    @FXML
+    private TextField producerTxtField;
+
+    public void addNewIngredient() {
+        Ingredient newIngredient = new Ingredient(nameTxtField.getText(), typeTxtField.getText(),
+                Integer.parseInt(priceTxtField.getText()), producerTxtField.getText());
+    }
+
+
 }
