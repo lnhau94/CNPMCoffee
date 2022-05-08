d82d69a1c3188b72d43d2c7d4d1e15d09f7d4d38
Hau <lnhau94@gmail.com>
2022-05-08 21:59:41 +0700

    color SignIn

--- MainApp.java
+++ MainApp.java
@@ -1,29 +1,28 @@
 package Main;
 
+import Main.Helpers.MainControl.ControlBar;
 import Main.Sales.Sales.Control.SalesApplicationControl;
-import Main.Sales.Sales.View.SalesApplicationView;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
 
 
 public class MainApp extends Application {
 
     Dimension screenSize;
 
-    BorderPane mainScreen;
     Stage toolPanel;
 
 
     private void createControlBar(Stage stage){
+        screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         toolPanel = new Stage(StageStyle.TRANSPARENT);
         toolPanel.setScene(new Scene(new ControlBar(stage)));
         ((ControlBar)toolPanel.getScene().getRoot()).prepareCSS();
@@ -40,13 +39,12 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        stage.initStyle(StageStyle.TRANSPARENT);
         SalesApplicationControl control = new SalesApplicationControl();
-        stage.setScene(control.getView());
-
+        //stage.setScene(control.getView());
+        stage.setScene(new Scene(FXMLLoader.load(new File("Helpers/SignIn/SignIn.fxml").toURI().toURL())));
         createControlBar(stage);
         toolPanel.initOwner(stage);
-        stage.setResizable(false);
         stage.show();
         toolPanel.show();
     }
